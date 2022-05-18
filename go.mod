47b55f08d1cd5313c8b38c0518ebacd5f2e34803
邓捷 <dj@lifesdk.com>
2022-05-18 19:30:42 +0800

    init structure

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Cyvadra/binance-local
+
+go 1.16
+
+require (
+	github.com/gin-contrib/gzip v0.0.5
+	github.com/gin-gonic/gin v1.7.7
+)
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gin-contrib/gzip"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	gin.SetMode(gin.DebugMode)
+	g := gin.Default()
+	// empty returns
+	g.GET("/", func(c *gin.Context) { c.String(200, "") })
+	g.GET("/robots.txt", func(c *gin.Context) { c.String(200, "User-agent: *\nDisallow: *") })
+	g.GET("/sitemap.xml", func(c *gin.Context) { c.String(200, "") })
+	g.GET("/favicon.ico", func(c *gin.Context) { c.String(404, "") })
+	default404str := strings.Repeat("00000000", 1024)
+	g.NoRoute(gzip.Gzip(gzip.DefaultCompression), func(c *gin.Context) { c.String(200, default404str) })
+	// pull up service
+	g.Run("127.0.0.1:8023")
+	fmt.Println("server up")
+}
